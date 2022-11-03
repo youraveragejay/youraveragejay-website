6382969ad8de95ff86b741237451e39984813f89
youraveragejay <98917885+youraveragejay@users.noreply.github.com>
2022-11-03 13:48:30 +0000

    projects title

--- pages/projects.js
+++ pages/projects.js
@@ -1,8 +1,12 @@
 import MainLayout from "../layouts/MainLayout";
+import Head from "next/head"
 
 const Contact = () => {
   return (
     <MainLayout>
+      <Head>
+        <title>youraveragejay | Projects</title>
+      </Head>
       <div>
         <h1>My Projects</h1>
       </div>
