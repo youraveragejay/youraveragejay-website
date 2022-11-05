abef7d6731029c9fa96e9739a0c80a8e0684473a
youraveragejay <jcracknell08@gmail.com>
2022-11-05 11:53:57 +0000

    next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-};
-
-module.exports = {
-  nextConfig,
   async redirects() {
     return [
       {
